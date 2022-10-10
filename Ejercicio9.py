e17325fa75ce765798b1c91336751a986fc79a74
aatondouna <113340871+aatondouna@users.noreply.github.com>
2022-10-10 12:06:04 +0200

    Ejercicio9 sin completar

---
+++ Ejercicio9.py
@@ -0,0 +1,13 @@
+fecha_nacimiento = input("Introduce fecha de nacimiento (dd/mm/aaaa)\n")
+
+fecha = fecha_nacimiento.split("/")
+
+print("Dia:", fecha)
+print("Mes:", fecha)
+print("Año:", fecha)
+
+
+
+
+
+
