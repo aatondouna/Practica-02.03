64a4eb733b9a958f9bd7e55452e15320f9295450
aatondouna <113340871+aatondouna@users.noreply.github.com>
2022-10-10 11:18:31 +0200

    Ejercicio7 completado

---
+++ Ejercicio7.py
@@ -0,0 +1,26 @@
+correo_electronico = input("¿Cual es tu correo electronico?\n")
+
+lst_cadena = correo_electronico.split("@")
+
+print(correo_electronico.split("@")[0] + "ceu.es")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
