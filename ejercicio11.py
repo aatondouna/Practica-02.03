5410881652fde05ecb0c00d445e09da81746a0b4
juan miguel <jlojimeagu1@educacion.navarra.es>
2022-10-11 18:35:26 +0200

    ejercicio11 completado

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,6 +1,7 @@
 producto = input("Introduzca el nombre del producto:\n")
 precio = float(input("Introduzca el precio:\n"))
 unidad = float(input("Introduzca la cantidad:\n"))
-total = (precio * unidad)
-print("Tu producto es", producto, ", el precio unitario es de{precio:6.2f}€, el total de unidad es de{unidad:3.0f}, el total seria de{total:8.2f}€"
-     .format(producto = producto, precio = precio, unidad = unidad, total = total))
+total = precio * unidad
+print("Tu compra es", producto, "el valor es de{precio:6.2f}€:\n "
+                                "el total de unidad es de:{unidad:3.0f}\n el total seria de:{total:8.2f}€"
+      .format(producto=producto, precio=precio, unidad=unidad, total=total))
